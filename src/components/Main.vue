72acc19451a82c46d77ec3036836fda8c626fb98
716green <bob@bbass.co>
2020-09-13 09:00:59 -0400

    first commit

---
+++ src/components/Main.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <h4>{{title}}</h4>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="currentBalance"
+            :value="currentBalance"
+            label="Current Balance"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field v-model="fees" :value="fees" label="Fees" required></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field v-model="downPayment" :value="downPayment" label="Down Payment" required></v-text-field>
+        </v-col>
+        Remaining Balance: {{this.remainingBalance}}
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {
+    title: {
+      type: String,
+    },
+  },
+  data: () => ({
+    currentBalance: 0,
+    fees: 0,
+    downPayment: 0,
+    installmentAmount: 0,
+    installmentCount: 0,
+  }),
+  computed: {
+    remainingBalance() {
+      return (
+        "$" +
+        (parseInt(this.currentBalance) +
+          parseInt(this.fees) -
+          parseInt(this.downPayment))
+      );
+    },
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.svg" width="20%" />
+    <Main title="Calculator" />
+  </div>
+</template>
+
+<script>
+import Main from "@/components/Main.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Main,
+  },
+};
+</script>
+<style>
+* {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
