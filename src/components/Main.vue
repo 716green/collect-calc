392108210948bf12bc8d3db6565c2a670d3be420
716green <bob@bbass.co>
2020-09-15 12:26:45 -0400

    finished working prototype

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,21 +5,93 @@
       <v-row>
         <v-col cols="12" md="4">
           <v-text-field
+            @keypress="isNumber($event)"
             v-model="currentBalance"
             :value="currentBalance"
             label="Current Balance"
+            prepend-icon="mdi-currency-usd"
             required
           ></v-text-field>
         </v-col>
 
         <v-col cols="12" md="4">
-          <v-text-field v-model="fees" :value="fees" label="Fees" required></v-text-field>
+          <v-text-field
+            @keypress="isNumber($event)"
+            v-model="fees"
+            :value="fees"
+            label="Fees"
+            prepend-icon="mdi-currency-usd"
+            required
+          ></v-text-field>
         </v-col>
 
         <v-col cols="12" md="4">
-          <v-text-field v-model="downPayment" :value="downPayment" label="Down Payment" required></v-text-field>
+          <v-text-field
+            @keypress="isNumber($event)"
+            v-model="downPayment"
+            :value="downPayment"
+            label="Down Payment"
+            prepend-icon="mdi-currency-usd"
+            required
+          ></v-text-field>
         </v-col>
-        Remaining Balance: {{this.remainingBalance}}
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="remaining"
+            :value="remaining"
+            label="Remaining"
+            prepend-icon="mdi-currency-usd"
+            required
+            disabled
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            @keypress="isNumber($event)"
+            v-model="installmentAmount"
+            :value="installmentAmount"
+            label="Installment Amount"
+            prepend-icon="mdi-currency-usd"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            @keypress="isNumber($event)"
+            v-model="installmentCount"
+            :value="installmentCount"
+            label="Installment Count"
+            prepend-icon="mdi-pound"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            @keypress="isNumber($event)"
+            v-model="installmentTotal"
+            :value="installmentTotal"
+            label="Installment Total"
+            prepend-icon="mdi-currency-usd"
+            required
+            disabled
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            @keypress="isNumber($event)"
+            v-model="remainingBalance"
+            :value="remainingBalance"
+            label="Remaining Balance"
+            prepend-icon="mdi-currency-usd"
+            required
+            disabled
+          ></v-text-field>
+        </v-col>
       </v-row>
     </v-container>
   </v-form>
@@ -34,20 +106,51 @@ export default {
     },
   },
   data: () => ({
-    currentBalance: 0,
-    fees: 0,
-    downPayment: 0,
-    installmentAmount: 0,
-    installmentCount: 0,
+    currentBalance: null,
+    fees: null,
+    downPayment: null,
+    installmentAmount: null,
   }),
   computed: {
+    installmentCount() {
+      if (this.installmentAmount) {
+        return parseInt(this.remaining) / parseInt(this.installmentAmount);
+      } else {
+        return 0;
+      }
+    },
+    remaining() {
+      return (
+        parseInt(this.currentBalance) +
+        parseInt(this.fees) -
+        parseInt(this.downPayment)
+      );
+    },
+    installmentTotal() {
+      return parseInt(this.installmentAmount) * parseInt(this.installmentCount);
+    },
     remainingBalance() {
       return (
-        "$" +
-        (parseInt(this.currentBalance) +
-          parseInt(this.fees) -
-          parseInt(this.downPayment))
+        parseInt(this.currentBalance) +
+        parseInt(this.fees) -
+        parseInt(this.downPayment) -
+        parseInt(this.installmentTotal)
       );
+    },
+  },
+  methods: {
+    isNumber: function (evt) {
+      evt = evt ? evt : window.event;
+      var charCode = evt.which ? evt.which : evt.keyCode;
+      if (
+        charCode > 31 &&
+        (charCode < 48 || charCode > 57) &&
+        charCode !== 46
+      ) {
+        evt.preventDefault();
+      } else {
+        return true;
+      }
     },
   },
 };
